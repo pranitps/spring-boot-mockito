591ac60002c28f21a753a77f01e736d89da9e709
pranitps <pranitps@gmail.com>
2021-12-24 15:09:24 -0500

    Added more test cases

--- spring-boot-mockito/src/main/java/com/library/junit/springboottest/BookController.java
+++ spring-boot-mockito/src/main/java/com/library/junit/springboottest/BookController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,43 +21,49 @@ import javassist.NotFoundException;
 @RequestMapping(value = "/book")
 public class BookController {
 
-    @Autowired
-    BookRepository bookRepository;
+	@Autowired
+	BookRepository bookRepository;
 
-    @GetMapping
-    public List<Book> getAllBookRecords() {
-        return bookRepository.findAll();
-    }
+	@GetMapping
+	public List<Book> getAllBookRecords() {
+		return bookRepository.findAll();
+	}
 
-    @GetMapping(value = "{bookId}")
-    public Book getBookById(@PathVariable(value = "bookId") Long bookId) {
-        return bookRepository.findById(bookId).get();
-    }
+	@GetMapping(value = "{bookId}")
+	public Book getBookById(@PathVariable(value = "bookId") Long bookId) {
+		return bookRepository.findById(bookId).get();
+	}
 
-    @PostMapping
-    public Book createBookRecord(@RequestBody @Valid Book bookRecord) {
-        return bookRepository.save(bookRecord);
-    }
+	@PostMapping
+	public Book createBookRecord(@RequestBody @Valid Book bookRecord) {
+		return bookRepository.save(bookRecord);
+	}
 
-    @PutMapping
-    public Book updateBookRecord(@RequestBody @Valid Book bookRecord) throws NotFoundException{
-        if (bookRecord == null || bookRecord.getBookId() == null) {
-            throw new NotFoundException("BookRecord or ID cannot be null");
-        }
-        Optional<Book> optionalBook = bookRepository.findById(bookRecord.getBookId());
+	@PutMapping
+	public Book updateBookRecord(@RequestBody @Valid Book bookRecord) throws NotFoundException {
+		if (bookRecord == null || bookRecord.getBookId() == null) {
+			throw new NotFoundException("BookRecord or ID cannot be null");
+		}
+		Optional<Book> optionalBook = bookRepository.findById(bookRecord.getBookId());
 
-        if(!optionalBook.isPresent()){
-            throw new NotFoundException("Book with Id: " + bookRecord.getBookId() + " not found.");
-        }
+		if (!optionalBook.isPresent()) {
+			throw new NotFoundException("Book with Id: " + bookRecord.getBookId() + " not found.");
+		}
 
-        Book exisitingBookRec = optionalBook.get();
-        exisitingBookRec.setName(bookRecord.getName());
-        exisitingBookRec.setSummary(bookRecord.getSummary());
-        exisitingBookRec.setRating(bookRecord.getRating());
+		Book exisitingBookRec = optionalBook.get();
+		exisitingBookRec.setName(bookRecord.getName());
+		exisitingBookRec.setSummary(bookRecord.getSummary());
+		exisitingBookRec.setRating(bookRecord.getRating());
 
-        return bookRepository.save(exisitingBookRec);
-    }
+		return bookRepository.save(exisitingBookRec);
+	}
 
+	@DeleteMapping(value = "{bookId}")
+	public void deleteBookById(@PathVariable(value = "bookId") Long bookId) throws NotFoundException {
+		if (!bookRepository.findById(bookId).isPresent()) {
+			throw new NotFoundException("Book Id " + bookId + " not present");
+		}
 
+		bookRepository.deleteById(bookId);
+	}
 }
-
--- spring-boot-mockito/src/test/java/com/library/junit/springboottest/BookControllerTest.java
+++ spring-boot-mockito/src/test/java/com/library/junit/springboottest/BookControllerTest.java
@@ -1,11 +1,14 @@
 package com.library.junit.springboottest;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -24,66 +27,88 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 
+import javassist.NotFoundException;
+
 @WebMvcTest(BookController.class)
 public class BookControllerTest {
 
-    @Autowired
-    private MockMvc mockMvc;
+	@Autowired
+	private MockMvc mockMvc;
 
-    ObjectMapper objectMapper = new ObjectMapper();
-    ObjectWriter objectWriter = objectMapper.writer();
+	ObjectMapper objectMapper = new ObjectMapper();
+	ObjectWriter objectWriter = objectMapper.writer();
 
-    @MockBean
-    private BookRepository bookRepository;
+	@MockBean
+	private BookRepository bookRepository;
 
-    Book book_rec_1 = new Book(1L, "book1", "book summary 1", 1);
-    Book book_rec_2 = new Book(2L, "book2", "book summary 2", 2);
-    Book book_rec_3 = new Book(3L, "book3", "book summary 3", 3);
+	Book book_rec_1 = new Book(1L, "book1", "book summary 1", 1);
+	Book book_rec_2 = new Book(2L, "book2", "book summary 2", 2);
+	Book book_rec_3 = new Book(3L, "book3", "book summary 3", 3);
 
-    @Test
-    public void getAllListOfBooks_success() throws Exception {
-        List<Book> bookList = new ArrayList<>(Arrays.asList(book_rec_1, book_rec_2, book_rec_3));
+	@Test
+	public void getAllListOfBooks_success() throws Exception {
+		List<Book> bookList = new ArrayList<>(Arrays.asList(book_rec_1, book_rec_2, book_rec_3));
 
-        when(bookRepository.findAll()).thenReturn(bookList);
+		when(bookRepository.findAll()).thenReturn(bookList);
 
-        mockMvc.perform(get("/book")
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(MockMvcResultMatchers.jsonPath("$", hasSize(3)))
-                .andExpect(jsonPath("$[2].name", is("book3")));
-    }
+		mockMvc.perform(get("/book").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
+				.andExpect(MockMvcResultMatchers.jsonPath("$", hasSize(3)))
+				.andExpect(jsonPath("$[2].name", is("book3")));
+	}
 
-    @Test
-    public void getBookById_success() throws Exception {
-        when(bookRepository.findById(book_rec_1.getBookId())).thenReturn(java.util.Optional.of(book_rec_1));
+	@Test
+	public void getBookById_success() throws Exception {
+		when(bookRepository.findById(book_rec_1.getBookId())).thenReturn(java.util.Optional.of(book_rec_1));
 
-        mockMvc.perform(get("/book/1")
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$", notNullValue()))
-                .andExpect(jsonPath("$.name", is("book1")));
-    }
+		mockMvc.perform(get("/book/1").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
+				.andExpect(jsonPath("$", notNullValue())).andExpect(jsonPath("$.name", is("book1")));
+	}
 
-    @Test
-    public void createBookRec_success() throws Exception {
-        Book book = Book.builder()
-                .bookId(4L)
-                .name("Book 4")
-                .summary("Book Summary 4")
-                .rating(4).build();
+	@Test
+	public void getBookById_exception() throws Exception {
+		when(bookRepository.findById(4L)).thenReturn(java.util.Optional.of(book_rec_1));
 
-        when(bookRepository.save(book)).thenReturn(book);
+		mockMvc.perform(get("/book/4").contentType(MediaType.APPLICATION_JSON))
+				.andExpect(result -> assertThat(result.getResolvedException() instanceof NotFoundException));
+	}
 
-        String content = objectWriter.writeValueAsString(book);
+	@Test
+	public void createBookRec_success() throws Exception {
+		Book book = Book.builder().bookId(4L).name("Book 4").summary("Book Summary 4").rating(4).build();
 
-        mockMvc.perform(post("/book")
-                .contentType(MediaType.APPLICATION_JSON)
-                .accept(MediaType.APPLICATION_JSON)
-                .content(content))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$", notNullValue()))
-                .andExpect(jsonPath("$.name", is("Book 4")));;
+		when(bookRepository.save(book)).thenReturn(book);
 
+		String content = objectWriter.writeValueAsString(book);
 
-    }
+		mockMvc.perform(post("/book").contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON)
+				.content(content)).andExpect(status().isOk()).andExpect(jsonPath("$", notNullValue()))
+				.andExpect(jsonPath("$.name", is("Book 4")));
+
+	}
+
+	@Test
+	public void updateBookRecord_success() throws Exception {
+
+		Book updatedBookRec = Book.builder().bookId(1L).name("New Book name for Book 1").summary("Updated book 1")
+				.rating(5).build();
+
+		when(bookRepository.findById(book_rec_1.getBookId())).thenReturn(java.util.Optional.of(book_rec_1));
+		when(bookRepository.save(updatedBookRec)).thenReturn(updatedBookRec);
+
+		String updatedContent = objectWriter.writeValueAsString(updatedBookRec);
+
+		mockMvc.perform(put("/book").contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON)
+				.content(updatedContent)).andExpect(status().isOk()).andExpect(jsonPath("$", notNullValue()))
+				.andExpect(jsonPath("$.name", is("New Book name for Book 1")));
+
+	}
+	
+	@Test
+	public void deleteBookRecord_success() throws Exception {
+		when(bookRepository.findById(book_rec_2.getBookId())).thenReturn(java.util.Optional.of(book_rec_2));
+		
+		mockMvc.perform(delete("/book/2")
+				.contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk());
+	}
 }
